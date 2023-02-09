52b1e17918d38b13f18f1bc9f24241c1cf306215
Jus7Margata <marten2dobroykov@gmail.com>
2023-02-09 09:31:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uchilishte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете колко дни е работил в месеца: ");
+            double dni = int.Parse(Console.ReadLine());
+
+            Console.Write("Въведете колко пари изкарва на ден: ");
+            double pariden = int.Parse(Console.ReadLine());
+
+            Console.Write("Курс на долара спрямо ЛВ: ");
+            double kurs = double.Parse(Console.ReadLine());
+
+            double mesec = dni * pariden;
+            double godina = (mesec * 12) + (mesec * 2.5);
+            double danuk = godina / 4;
+            double chistdohod = godina - danuk;
+            double pechalba = chistdohod / 365;
+            double pechalbalv = pechalba * kurs;
+
+            Console.WriteLine("Средната печалба на ден е: " + Math.Round(pechalbalv, 2));
+
+        }
+    }
+}
